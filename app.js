3a27c5418f00ae02784153bddc419607ff6d77f8
Rob Chambers <robchamberspfc@gmail.com>
2024-02-29 15:11:12 +0000

    fix url

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -7,6 +8,13 @@ const routes = require('./routes');
 app.use('/v1', routes);
 
 app.use('/data', express.static('content'))
+app.use('/diagram', express.static('images'))
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', async(req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
